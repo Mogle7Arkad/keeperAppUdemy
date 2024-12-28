a55c87ebfd44933932e1ba29951ab20f16f33ab1
Ali George <george7ali@gmail.com>
2024-12-28 11:06:09 +0100

    create the note compnent and export it to the app component

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDom from "react-dom"
 import Header from "./Header";
 import Footer from "./Footer";
+import Note from "./Note";
 
 function App(){
     return <div className="body">
         <Header />
+        <Note />
         <Footer />
     </div>
 }
