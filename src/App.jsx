d7c672c4302443d93f8bc0e09511c10c3254fb47
Ali George <george7ali@gmail.com>
2024-12-28 10:33:36 +0100

    create the header compnent and export it to the app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDom from "react-dom"
+import Header from "./Header";
 
 function App(){
-    return <div>hello</div>
+    return <div className="body">
+        <Header />
+    </div>
 }
 
 export default App
